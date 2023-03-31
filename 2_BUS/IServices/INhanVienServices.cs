a24a452d527f73619a746366a21e3498f7df7f0a
ANTHONYPC\Anthony <AnthonyNguyenReal@gmail.com>
2023-03-31 13:52:17 +0700

    the second commit include service nhanvien

---
+++ 2_BUS/IServices/INhanVienServices.cs
@@ -0,0 +1,17 @@
+﻿using _1_DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS.IServices
+{
+    public interface INhanVienServices
+    {
+        bool Add(NhanVien obj);
+        bool Update(NhanVien obj);
+        bool Delete(NhanVien obj);
+        List<NhanVien> GetAll();
+    }
+}
---
+++ 2_BUS/Services/NhanVienServices.cs
@@ -0,0 +1,40 @@
+﻿using _1_DAL.Entities;
+using _1_DAL.IRepository;
+using _1_DAL.Repository;
+using _2_BUS.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS.Services
+{
+    public class NhanVienServices : INhanVienServices
+    {
+        private INhanVienRepository _inhanvienrepository = new NhanVienRepository();
+        public bool Add(NhanVien obj)
+        {
+            _inhanvienrepository.Add(obj);
+            return true;    
+        }
+
+        public bool Delete(NhanVien obj)
+        {
+            _inhanvienrepository.Delete(obj);
+            return true;
+        }
+
+        public List<NhanVien> GetAll()
+        {
+            return _inhanvienrepository.GetAll();
+           
+        }
+
+        public bool Update(NhanVien obj)
+        {
+            _inhanvienrepository.Update(obj);
+            return true;
+        }
+    }
+}
